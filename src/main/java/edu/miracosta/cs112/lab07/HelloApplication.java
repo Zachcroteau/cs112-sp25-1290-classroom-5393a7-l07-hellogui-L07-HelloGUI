df699c28bebb2e5e613af61ac9874e9d06d83ebf
Zachcroteau <zachcroteau@gmail.com>
2025-04-17 01:52:35 +0000

    HEY! Just copied and pasted this into here. My other labs look like they're uploaded properly idk what was happening with this one.

--- src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
+++ src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
@@ -1,26 +1,83 @@
-package edu.miracosta.cs112.lab07;//package name here depending on your IDE
+package edu.miracosta.cs112.lab07;
 
-import javafx.application.Application;  //abstract class used for JavaFX GUI's
-import javafx.stage.Stage;              //class for GUI window
-import javafx.scene.Scene;              //class for specific view in GUI window
-import javafx.scene.layout.VBox;        //class for layout pane, organized top-to-bottom
-import javafx.scene.control.Label;      //class for label component
-import javafx.scene.control.Button;     //class for button component
-import javafx.event.EventHandler;       //interface for handling events
-import javafx.event.ActionEvent;        //class for type of event for action (like button or key pressed)
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.stage.Stage;              
+import javafx.scene.Scene;              
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;     
+import javafx.event.EventHandler;            
+import javafx.scene.layout.Pane;
+import javafx.scene.input.MouseEvent;
 
-public class HelloApplication extends Application  { //inheriting core functionality + this class will handle events
+
+
+public class HelloApplication extends Application implements ChangeListener<Number>, EventHandler<MouseEvent> { //inheriting core functionality + this class will handle events
     /*** GUI COMPONENTS ***/
-    // TODO: follow step 25 in README.md to create reference variables
+    int buttonPresses = 0; // counter for button presses
+    Button firstButton = new Button("First Button");
+    Button secondButton = new Button("Second Button");
+    Label label = new Label("Hello GUI World!");
+    Label label2 = new Label("Button Presses: " + buttonPresses);
+    Pane layout = new Pane();
+    Scene scene = new Scene(layout, 400, 300);
+    TextField textField = new TextField("Enter text here"); // TextField for user input
 
-    /*** DRIVER main ***/
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        layout.getChildren().addAll(label, label2, firstButton, secondButton, textField); // Add components to the layout
+        
+        stage.setScene(scene);
+        stage.setTitle("Hello GUI: Zachary Croteau"); 
+        stage.show();
+
+        scene.widthProperty().addListener(this);
+        scene.heightProperty().addListener(this);
+        firstButton.setOnMouseClicked(this);
+        secondButton.setOnMouseClicked(this);
+
+        label.setLayoutX(scene.getWidth() - label.getWidth() - 10);
+        label.setLayoutY(10);
+        firstButton.setLayoutY(scene.getHeight() - firstButton.getHeight() - 10);
+        firstButton.setLayoutX(10);
+        secondButton.setLayoutY(scene.getHeight() - secondButton.getHeight() - 10);
+        secondButton.setLayoutX(scene.getWidth() - secondButton.getWidth() - 10);
+
+        textField.setLayoutX(scene.getWidth() / 2 - textField.getWidth() / 2);
+        textField.setLayoutY(scene.getHeight() / 2 - textField.getHeight() / 2 );
+        label2.setLayoutX(10);
+        label2.setLayoutY(10);
+        
+        
+        
+      }  
+    
+    @Override
+    public void changed(ObservableValue<? extends Number>obs, Number oldVal, Number newVal) {
+        label.setLayoutX(scene.getWidth() - label.getWidth() - 10);
+        secondButton.setLayoutX(scene.getWidth() - secondButton.getWidth() - 10);
+        firstButton.setLayoutY(scene.getHeight() - firstButton.getHeight() - 10);
+        secondButton.setLayoutY(scene.getHeight() - secondButton.getHeight() - 10);
+        textField.setLayoutX(scene.getWidth() / 2 - textField.getWidth() / 2);
+        textField.setLayoutY(scene.getHeight() / 2 - textField.getHeight() / 2 );
+    }
+    @Override
+    public void handle(MouseEvent event) {
+        if (event.getSource() == firstButton) {
+            label.setText(textField.getText());
+        } else if (event.getSource() == secondButton) {
+            buttonPresses++;
+            label2.setText("Button Presses: " + buttonPresses);
+        }
+    }
+    
     public static void main(String[] args) {
-        launch(args); //method from Application class, must be called to setup javafx application
+        launch(args); 
     }
 
-    // TODO: follow steps 2-9 in README.md to create a start method
 
-    // TODO: follow steps 10-21 in README.md to add objects to your layout (inside start)
-
-    // TODO: follow steps 22-34 in README.md to create an event handler
+    
 }
